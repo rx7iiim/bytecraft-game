0fc4952b35ae548efcb36cfa7fe666f10ee4c52a
rahim <154564602+rx7iiim@users.noreply.github.com>
2024-10-13 00:03:24 +0100

    just some changes on the getnewfruit everything is working

--- server/app.js
+++ server/app.js
@@ -40,18 +40,7 @@ app.use(cors(corsOptions));
 
 
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
+
 
 // Routes which should handle requests
 app.use("/user",userRoutes);
